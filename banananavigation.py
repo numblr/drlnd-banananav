efdf46ecd38cdc6ebd196d614edb20167cd47e89
Thomas Baier <thomas.baier@luminis.eu>
2019-01-25 20:19:47 +0100

    Switch from test environment to BananaEnv

--- banananavigation.py
+++ banananavigation.py
@@ -6,16 +6,15 @@ from banananav.environment import BananaEnv, BananaAgent, TestEnv
 from banananav.training import DeepQLearner
 from banananav.replaymemory import ReplayMemory
 
-env = TestEnv()
-# learner = DeepQLearner(env=env)
-learner = DeepQLearner(env=env)
+learner = DeepQLearner()
 scores_window = deque(maxlen=100)
-for cnt, data in enumerate(learner.train(2000)):
+for cnt, data in enumerate(learner.train(200)):
     loss, score, terminal = data
     if terminal:
         scores_window.append(score)
         pprint("{} - loss: {:+.3f} / score: {:+.3f}({:+.3f})".format(cnt, loss, np.mean(scores_window), score))
 
+env = BananaEnv()
 episode = env.generate_episode(learner.get_agent())
 for count, step_data in enumerate(episode):
     pprint("step: " + str(count))
